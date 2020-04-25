f15d478230d12f634b02a50c8c63de6e5bc7ddf7
spieljs <spieljs@gmail.com>
2020-04-25 19:44:26 +0200

    remove install wasm-bind-bindgen

--- ci/run_checks.sh
+++ ci/run_checks.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
 cargo fmt --manifest-path=crate/yew_styles/Cargo.toml --all -- --check
 cargo clippy --manifest-path=crate/yew_styles/Cargo.toml --all -- --deny=warnings
 cargo test --target wasm32-unknown-unknown --manifest-path=crate/yew_styles/Cargo.toml
