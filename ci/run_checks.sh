d09cd8c1c7af76f521c06f65b3a19e904269cc17
spieljs <spieljs@gmail.com>
2020-06-11 00:34:34 +0200

    fix ci

--- ci/run_checks.sh
+++ ci/run_checks.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 set -euxo pipefail
 
 cargo fmt --manifest-path=crate/yew_styles/Cargo.toml --all -- --check
 cargo clippy --manifest-path=crate/yew_styles/Cargo.toml --all --all-features -- --deny=warnings
-cargo test --target wasm32-unknown-unknown --all-features --manifest-path=crate/yew_styles/Cargo.toml
+CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUNNER=wasm-bindgen-test-runner cargo test --target wasm32-unknown-unknown --all-features --manifest-path=crate/yew_styles/Cargo.toml
