6ccfb4510641473618f58f143debf3e98392f766
spieljs <spieljs@gmail.com>
2020-06-11 00:05:44 +0200

    fix ci

--- ci/run_checks.sh
+++ ci/run_checks.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 cargo fmt --manifest-path=crate/yew_styles/Cargo.toml --all -- --check
-cargo clippy --manifest-path=crate/yew_styles/Cargo.toml --all -- --deny=warnings
+cargo clippy --manifest-path=crate/yew_styles/Cargo.toml --all --all-features -- --deny=warnings
 cargo test --target wasm32-unknown-unknown --all-features --manifest-path=crate/yew_styles/Cargo.toml
