97592cd2b85ed72e955b0f43ab63846100d235e8
spieljs <spieljs@gmail.com>
2020-06-11 00:42:23 +0200

    add env variable

--- ci/run_checks.sh
+++ ci/run_checks.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 cargo fmt --manifest-path=crate/yew_styles/Cargo.toml --all -- --check
 cargo clippy --manifest-path=crate/yew_styles/Cargo.toml --all --all-features -- --deny=warnings
-CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUNNER=wasm-bindgen-test-runner cargo test --target wasm32-unknown-unknown --all-features --manifest-path=crate/yew_styles/Cargo.toml
+cargo test --target wasm32-unknown-unknown --all-features --manifest-path=crate/yew_styles/Cargo.toml
