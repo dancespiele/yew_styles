6ac87ca0e8aa7134594ca4cee159f705a44acb54
spieljs <spieljs@gmail.com>
2020-06-11 01:54:13 +0200

    export variable

--- ci/run_checks.sh
+++ ci/run_checks.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+export CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUNNER=wasm-bindgen-test-runner
 
 cargo fmt --manifest-path=crate/yew_styles/Cargo.toml --all -- --check
 cargo clippy --manifest-path=crate/yew_styles/Cargo.toml --all --all-features -- --deny=warnings
