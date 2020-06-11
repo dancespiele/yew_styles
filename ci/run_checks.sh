e85dcc832a4623efb972a8e868dd626289ad7625
spieljs <spieljs@gmail.com>
2020-06-11 03:32:58 +0200

    fix clippy command

--- ci/run_checks.sh
+++ ci/run_checks.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 
 cargo fmt --manifest-path=crate/yew_styles/Cargo.toml --all -- --check
-cargo clippy --manifest-path=crate/yew_styles/Cargo.toml --all --all-features --deny=warnings
+cargo clippy --manifest-path=crate/yew_styles/Cargo.toml --all --all-features -- --deny=warnings
 wasm-pack test --headless --firefox crate
