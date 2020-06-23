1ec594dbd965913983f6f6cc4350dec769e2cca3
spieljs <spieljs@gmail.com>
2020-06-24 01:35:11 +0200

    include two more icons

--- crate/yew_styles/src/assets.rs
+++ crate/yew_styles/src/assets.rs
@@ -4,6 +4,8 @@ use yew::prelude::*;
 #[derive(Clone)]
 pub enum Icon {
     Menu,
+    Plus,
+    Edit,
 }
 
 pub enum Msg {}
@@ -55,12 +57,15 @@ impl Component for Assets {
 fn get_icon(icon: Icon, class_name: String, id: String) -> Html {
     match icon {
         Icon::Menu => get_menu(class_name, id),
+        Icon::Edit => get_edit(class_name, id),
+        Icon::Plus => get_plus(class_name, id),
     }
 }
 
 fn get_menu(class_name: String, id: String) -> Html {
     html! {
-        <svg class=class_name
+        <svg
+            class=class_name
             id=id
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 512 512">
@@ -101,6 +106,48 @@ fn get_menu(class_name: String, id: String) -> Html {
                         403.00,362.11 203.00,362.11 203.00,362.11
                         203.00,362.11 137.00,362.11 137.00,362.11
                         137.00,362.11 101.00,362.11 101.00,362.11 Z" />
+        </svg>
+    }
+}
+
+fn get_edit(class_name: String, id: String) -> Html {
+    html! {
+        <svg
+            id=id
+            class=class_name
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7">
+                </path>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z">
+                </path>
+        </svg>
+    }
+}
+
+fn get_plus(class_name: String, id: String) -> Html {
+    html! {
+        <svg
+            id=id
+            class=class_name
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round">
+                <line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12">
+                </line>
         </svg>
     }
 }
